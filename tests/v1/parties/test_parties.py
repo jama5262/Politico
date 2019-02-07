204f0a675ae1f7986874b3b3067fd3cc371c9a67
Jama Mohamed <jama3137@gmail.com>
2019-02-07 23:43:36 +0300

    [Finishes #163800404] Finished TD for party endpoint

--- tests/v1/parties/test_parties.py
+++ tests/v1/parties/test_parties.py
@@ -3,17 +3,23 @@ import json
 from app import createApp
 
 
-class TesParties(unittest.TestCase):
+class TestParties(unittest.TestCase):
     def setUp(self):
         self.app = createApp("testing")
         self.client = self.app.test_client()
-        self.partyID = 3
         self.data = {
-          "id": self.partyID,
+          "id": 3,
           "name": "Party Name",
           "abbr": "Party Abbreviation",
           "logoUrl": "Party URL",
           "hqAddress": "Party HQ",
+        }
+        self.dataUpdate = {
+          "id": None,
+          "name": "Updated Party Name",
+          "abbr": "Updated Party Abbreviation",
+          "logoUrl": "Updated Party URL",
+          "hqAddress": "Updated Party HQ",
         }
 
     def post(self, path):
@@ -22,8 +28,16 @@ class TesParties(unittest.TestCase):
     def get(self, path):
         return self.client.get(path=path, content_type='application/json')
 
+    def patch(self, path):
+        return self.client.patch(path=path, data=json.dumps(self.dataUpdate), content_type='application/json')
+
+    def delete(self, path):
+        return self.client.delete(path=path, content_type='application/json')
+
     def test_create_party(self):
         response = self.post("/api/v1/parties")
+        self.assertTrue(response.json["data"]["id"])
+        TestParties().partyID = response.json["data"]["id"]
         self.assertEqual(response.status_code, 201)
 
     def test_get_all_parties(self):
@@ -31,5 +45,17 @@ class TesParties(unittest.TestCase):
         self.assertEqual(response.status_code, 201)
 
     def test_get_specific_party(self):
-        response = self.get("/api/v1/parties/" + str(self.partyID))
+        postParty = self.post("/api/v1/parties")
+        response = self.get("/api/v1/parties/" + str(postParty.json["data"]["id"]))
+        self.assertEqual(response.status_code, 201)
+
+    def test_edit_specific_party(self):
+        postParty = self.post("/api/v1/parties")
+        self.dataUpdate["id"] = postParty.json["data"]["id"]
+        response = self.patch("/api/v1/parties/" + str(postParty.json["data"]["id"]))
+        self.assertEqual(response.status_code, 201)
+
+    def test_delete_specific_party(self):
+        postParty = self.post("/api/v1/parties")
+        response = self.delete("/api/v1/parties/" + str(postParty.json["data"]["id"]))
         self.assertEqual(response.status_code, 201)
