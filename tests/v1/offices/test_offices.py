6ae42111156e9b0f73095b23f51caf9c9993d587
Jama Mohamed <jama3137@gmail.com>
2019-02-08 10:42:51 +0300

    Updated the tests

--- tests/v1/offices/test_offices.py
+++ tests/v1/offices/test_offices.py
@@ -7,31 +7,36 @@ class TestOffice(unittest.TestCase):
     def setUp(self):
         self.app = createApp("testing")
         self.client = self.app.test_client()
+        self.officeID = 3
         self.data = {
-          "id": 2,
+          "id": 3,
           "type": "Office type",
           "name": "Office name"
         }
-        self.dataUpdate = {
-          "id": None,
-          "type": "Update Office type",
-          "name": "Update Office name"
+        self.dataNoNameProperty = {
+            "id": 3,
+            "type": "Office type",
+        }
+        self.dataEmptyValues = {
+          "id": 3,
+          "type": "",
+          "name": ""
         }
 
-    def post(self, path):
-        return self.client.post(path=path, data=json.dumps(self.data), content_type='application/json')
+    def tearDown(self):
+        self.app = None
+        self.client = None
+        self.data = {}
+        self.dataUpdate = {}
+
+    def post(self, path, data):
+        return self.client.post(path=path, data=json.dumps(data), content_type='application/json')
 
     def get(self, path):
         return self.client.get(path=path, content_type='application/json')
 
-    def patch(self, path):
-        return self.client.patch(path=path, data=json.dumps(self.dataUpdate), content_type='application/json')
-
-    def delete(self, path):
-        return self.client.delete(path=path, content_type='application/json')
-
     def test_create_party(self):
-        response = self.post("/api/v1/offices")
+        response = self.post("/api/v1/offices", self.data)
         self.assertTrue(response.json["data"]["id"])
         self.assertEqual(response.status_code, 201)
 
@@ -40,6 +45,13 @@ class TestOffice(unittest.TestCase):
         self.assertEqual(response.status_code, 201)
 
     def test_get_specific_party(self):
-        postParty = self.post("/api/v1/offices")
-        response = self.get("/api/v1/offices/" + str(postParty.json["data"]["id"]))
+        response = self.get("/api/v1/offices/" + str(self.officeID))
         self.assertEqual(response.status_code, 201)
+
+    def test_with_empty_values(self):
+        response = self.post("/api/v1/offices", self.dataEmptyValues)
+        self.assertEqual(response.status_code, 400)
+
+    def test_with_no_name_property(self):
+        response = self.post("/api/v1/offices", self.dataNoNameProperty)
+        self.assertEqual(response.status_code, 422)
