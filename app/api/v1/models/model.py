43326675708528b6047d310240eb0d6d5b6ba4b2
Jama Mohamed <jama3137@gmail.com>
2019-02-11 22:33:44 +0300

    [Start #163872271] Finished removing duplicate methods in the model module

--- app/api/v1/models/model.py
+++ app/api/v1/models/model.py
@@ -1,5 +1,5 @@
 import json
-from app.api.v1.validation import validation
+from app.api.v1.validation.validation import validate
 from app.api.v1.returnMessages import returnMessages
 
 dataStore = {
@@ -33,11 +33,6 @@ dataStore = {
   }
 }
 
-propertyData = {
-  "parties": ("id", "name", "hqAddress", "logoUrl", "abbr"),
-  "offices": ("id", "name", "type")
-}
-
 
 class Models():
     def __init__(self, tableName, data):
@@ -45,47 +40,34 @@ class Models():
         self.data = data
 
     def createData(self):
-        if validation.checkIfTableExitst(self.tableName) is False:
-            return returnMessages.error(404, "404 (Not Found), The requested URL was not found on the server. If you entered the URL manually please check your spelling and try again.")
-        elif validation.checkIfPropertiesExist(self.data, propertyData[self.tableName]) is False:
-            return returnMessages.error(422, "422 (Unprocessable Entity), Please make sure to enter the correct requests, which are " + str(propertyData[self.tableName]))
-        elif validation.checkIfPropertyValuesAreEmpty(self.data) is False:
-            return returnMessages.error(422, "422 (Unprocessable Entity), Please make sure the values are not empty, and that you have valid syntax")
-        elif validation.checkIfDataExists(dataStore[self.tableName], str(self.data["id"])) is True:
-            return returnMessages.error(403, "403 (Forbidden), You cannot create " + self.tableName + " that already exists")
+        valid = validate(dataStore, self.tableName, "c", self.data, str(self.data["id"]))
+        if valid["isValid"] is False:
+            return valid["data"]
         dataStore[self.tableName][str(self.data["id"])] = self.data
         return returnMessages.success(201, self.data)
 
     def getAllData(self):
-        if validation.checkIfTableExitst(self.tableName) is False:
-            return returnMessages.error(404, "404 (Not Found), The requested URL was not found on the server. If you entered the URL manually please check your spelling and try again.")
-        elif validation.checkIfAllDataExists(dataStore[self.tableName]) is False:
-            return returnMessages.error(404, "404 (Not Found), " + self.tableName + " data were not found")
+        valid = validate(dataStore, self.tableName, "r")
+        if valid["isValid"] is False:
+            return valid["data"]
         return returnMessages.success(201, dataStore[self.tableName])
 
     def getSpecificData(self, id):
-        if validation.checkIfTableExitst(self.tableName) is False:
-            return returnMessages.error(404, "404 (Not Found), The requested URL was not found on the server. If you entered the URL manually please check your spelling and try again.")
-        elif validation.checkIfDataExists(dataStore[self.tableName], str(id)) is False:
-            return returnMessages.error(404, "404 (Not Found), The " + self.tableName + " you are looking for does not exist")
+        valid = validate(dataStore, self.tableName, "rs", None, id)
+        if valid["isValid"] is False:
+            return valid["data"]
         return returnMessages.success(201, dataStore[self.tableName][str(id)])
 
     def editSpecificData(self, id):
-        if validation.checkIfTableExitst(self.tableName) is False:
-            return returnMessages.error(404, "404 (Not Found), The requested URL was not found on the server. If you entered the URL manually please check your spelling and try again.")
-        elif validation.checkIfPropertiesExist(self.data, propertyData[self.tableName]) is False:
-            return returnMessages.error(422, "422 (Unprocessable Entity), Please make sure to enter the correct requests, which are " + str(propertyData[self.tableName]))
-        elif validation.checkIfPropertyValuesAreEmpty(self.data) is False:
-            return returnMessages.error(422, "422 (Unprocessable Entity), Please make sure the values are not empty, and that you have valid syntax")
-        elif validation.checkIfDataExists(dataStore[self.tableName], id) is False:
-            return returnMessages.error(404, "404 (Not Found), The " + self.tableName + " you are trying to edit does not exist")
+        valid = validate(dataStore, self.tableName, "r", self.data, id)
+        if valid["isValid"] is False:
+            return valid["data"]
         dataStore[self.tableName][id]["name"] = self.data["name"]
         return returnMessages.success(201, dataStore[self.tableName][id])
 
     def deleteSpecificData(self, id):
-        if validation.checkIfTableExitst(self.tableName) is False:
-            return returnMessages.error(404, "404 (Not Found), The requested URL was not found on the server. If you entered the URL manually please check your spelling and try again.")
-        elif validation.checkIfDataExists(dataStore[self.tableName], id) is False:
-            return returnMessages.error(404, "404 (Not Found), The " + self.tableName + " you are trying to delete does not exist")
+        valid = validate(dataStore, self.tableName, "d", None, id)
+        if valid["isValid"] is False:
+            return valid["data"]
         dataStore[self.tableName].pop(id)
         return returnMessages.success(201, {"message": "Delete successful"})
