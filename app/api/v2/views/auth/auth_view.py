849e3c4da57511f6767f723defbfe1e9b27e6a0c
Jama Mohamed <jama3137@gmail.com>
2019-02-12 20:06:34 +0300

    [Finishes #163900216] Finshed user login api endpoint

--- app/api/v2/views/auth/auth_view.py
+++ app/api/v2/views/auth/auth_view.py
@@ -5,6 +5,12 @@ auth_view = Blueprint("auth_view", __name__)
 
 
 @auth_view.route("/signup", methods=["POST"])
-def createUser():
-    response = AuthModel(request.get_json(force=True)).createUser()
+def registerUser():
+    response = AuthModel(request.get_json(force=True)).registerUser()
+    return jsonify(response), response["status"]
+
+
+@auth_view.route("/login", methods=["POST"])
+def loginUser():
+    response = AuthModel(request.get_json(force=True)).loginUser()
     return jsonify(response), response["status"]
