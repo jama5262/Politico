ceeea86417856d43cd978a3a7214409d7a14ef41
Jama Mohamed <jama3137@gmail.com>
2019-02-19 16:33:12 +0300

    [Finishes #163900631] Finished generating access token

--- app/api/v2/views/auth/auth_view.py
+++ app/api/v2/views/auth/auth_view.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, jsonify, request
 from app.api.v2.models.auth.auth_model import AuthModel
+from flask_jwt_extended import create_access_token, jwt_required
 
 auth_view = Blueprint("auth_view", __name__)
 
@@ -7,10 +8,14 @@ auth_view = Blueprint("auth_view", __name__)
 @auth_view.route("/signup", methods=["POST"])
 def registerUser():
     response = AuthModel(request.get_json(force=True)).registerUser()
+    token = create_access_token(identity=response["data"]["user"]["email"])
+    response["data"]["token"] = token
     return jsonify(response), response["status"]
 
 
 @auth_view.route("/login", methods=["POST"])
 def loginUser():
     response = AuthModel(request.get_json(force=True)).loginUser()
+    token = create_access_token(identity=response["data"]["user"]["email"])
+    response["data"]["token"] = token
     return jsonify(response), response["status"]
