a5037f8963f612eff8f69d6a56f7b29999cb4dcd
Jama Mohamed <jama3137@gmail.com>
2019-02-28 19:45:51 +0300

    [Finishes #164300824] Finished user login

--- UI/api/fetch.js
+++ UI/api/fetch.js
@@ -1,20 +1,27 @@
 let baseUrl = "https://politico-andela-37.herokuapp.com/api/v2"
 
-let performFetch = (url, method="GET", data={}) => {
+let performFetch = (url, method="GET", data={}, authenticate=true) => {
   return new Promise(async (resolve, reject) => {
+    let token = "";
+    if (authenticate == true) {
+      try {
+        token = await readFromDatabase();
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    console.log(token);
     let fetchData = {
       method: method,
       headers: {
           "Content-Type": "application/json",
-          "Authorization": "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE1NTEzNjgxNDQsIm5iZiI6MTU1MTM2ODE0NCwianRpIjoiMmMyNmZlZjktZTVkMi00OTg4LTlkZjgtZDE0ZGY0ODg1NjA4IiwiZXhwIjoxNTUxMzY5MDQ0LCJpZGVudGl0eSI6eyJlbWFpbCI6ImFkbWluQGdtYWlsLmNvbSIsInJvbGUiOiJUcnVlIn0sImZyZXNoIjpmYWxzZSwidHlwZSI6ImFjY2VzcyJ9.SGAsBtHAwCcyIGssiELUjQRxry0c5fab2TsBhWJpQjI"
+          "Authorization": "Bearer " + token
       },
       body: JSON.stringify(data)
     }
-  
     if (method == "GET" || method == "DELETE") {
       delete fetchData.body;
     }
-  
     try {
       let response = await fetch(baseUrl + url, fetchData);
       let jsonObj = await response.json();
--- UI/assets/js/index.js
+++ UI/assets/js/index.js
@@ -1,12 +1,31 @@
 window.onload = () => {
   let loginBtn = document.getElementById("loginBtn");
   loginBtn.addEventListener("click", () => {
-    performFetch('/parties')
+    userData = {
+      email: "emai1@gmail.com",
+      password: "password1"
+    }
+    performFetch('/auth/login', "POST", userData, false)
     .then((data) => {
-      console.log(data);
+      if (data.status == 200) {
+        console.log(data);
+        return writeToDatabase({
+          user: "1",
+          token: data.data.token
+        });
+      } else {
+        console.log(data);
+      }
+    })
+    .then((data) => {
+      if (data != null) {
+        console.log(data);
+      } else {
+        console.log("not saved")
+      }
     })
     .catch((error) => {
       console.log(error);
     })
-  })
+  });
 }
