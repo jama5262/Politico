b383481387265b71476d3818ebe74d6ccd3a0ac2
Jama Mohamed <jama3137@gmail.com>
2019-03-04 11:02:42 +0300

    [Finishes #164312240] Finished admin party edit

--- UI/admin/adminParties/js/main.js
+++ UI/admin/adminParties/js/main.js
@@ -1,5 +1,4 @@
 window.onload = () => {
-  let navInstance = new AdminNavigation("index.html", "../adminGovOffices/index.html", "../../index.html");
   let partyHolder = document.getElementsByClassName("party-table-holder")[0];
   class AdminParties {
     constructor() {
@@ -30,13 +29,13 @@ window.onload = () => {
             <td>${ data[i].hq_address }</td>
             <td>
               <div style="display: flex">
-                <a href="./editParty.html"><button class="button-design-edit">edit</button></a>
+                <a href="./editParty.html?partyID=${ data[i].id }"><button class="button-design-edit">edit</button></a>
                 <button class="button-design-delete">delete</button>
               </div>
             </td>
           </tr>
         `
-        tableBody.insertAdjacentHTML('beforeend', tableRow);
+        tableBody.insertAdjacentHTML('afterbegin', tableRow);
       }
     }
     async createParty() {
@@ -60,13 +59,67 @@ window.onload = () => {
         this.main().loading(false)
       }
     }
+    async getSpecificParty(partyID) {
+      return new Promise(async (resolve, reject) => {
+        try {
+          this.main().loading()
+          let fetchInstance = new Fetch(`/parties/${ partyID }`);
+          let party = await fetchInstance.performFetch();
+          resolve(party.data.data[0]);
+          this.main().loading(false);
+        } catch (error) {
+          console.log(error.error || error.message);
+          reject(error.error)
+          this.main().alertInstance(error.error || "An error occured, please try again later", true);
+          if (error.error != null && (error.error == "Your session has expired" || error.error == "No access token")) {
+            await this.main().alertInstance(error + ", please login to continue", true);
+            this.main().navInstance().logout();
+          }
+          this.main().loading(false);
+        }
+      });
+    }
+    async populatePartyToInput() {
+      try {
+        let partyID = new URL(window.location.href).searchParams.get("partyID");
+        let party = await partiesInstance.getSpecificParty(partyID)
+        this.name.value = party.name;
+        this.abbr.value = party.abbr;
+        this.logoUrl.value = party.logo_url;
+        this.hqAddress.value = party.hq_address;
+      } catch (error) {
+        // this.main().alertInstance(error + ", please login to continue", true);
+      }
+    }
+    async editParty() {
+      try {
+        let partyID = new URL(window.location.href).searchParams.get("partyID");
+        this.main().loading()
+        let fetchInstance = new Fetch(`/parties/${ partyID }`, "PATCH", {
+          name: this.name.value,
+          abbr: this.abbr.value,
+          logo_url: this.logoUrl.value,
+          hq_address: this.hqAddress.value
+        });
+        let data = await fetchInstance.performFetch();
+        this.main().alertInstance(data.data.msg);
+        this.main().loading(false);
+      } catch (error) {
+        if (error != null && (error == "Your session has expired" || error == "No access token")) {
+          await this.main().alertInstance(error + ", please login to continue", true);
+          this.main().adminNavInstance().logout();
+        }
+        this.errorMessageFunc(error.error || "An error occured, please try again later", "block");
+        this.main().loading(false)
+      }
+    }
     async getAllParites() {
       try {
         this.main().loading()
         let fetchInstance = new Fetch("/parties");
         let data = await fetchInstance.performFetch();
         this.main().alertInstance(data.data.msg);
-        console.log(data.data.msg);
+        console.log(data.data.data);
         this.populate(data.data.data);
         this.main().loading(false);
       } catch (error) {
@@ -85,6 +138,8 @@ window.onload = () => {
 
   if (partyHolder != null) {
     partiesInstance.getAllParites();
+  } else {
+    partiesInstance.populatePartyToInput();
   }
 
   let logout = document.getElementById("logout");
@@ -96,6 +151,13 @@ window.onload = () => {
   if (createParty != null) {
     createParty.addEventListener("click", () => {
       partiesInstance.createParty();
+    });
+  }
+
+  let editParty = document.getElementById("editParty");
+  if (editParty != null) {
+    editParty.addEventListener("click", async () => {
+      partiesInstance.editParty();
     });
   }
 
