5be24acee55df7fd96fcee77537a95858c3a0d42
Jama Mohamed <jama3137@gmail.com>
2019-03-05 22:07:31 +0300

    [Feature #164392936] Finished getting all votes

--- UI/users/myVotes/js/main.js
+++ UI/users/myVotes/js/main.js
@@ -2,6 +2,7 @@ window.onload = () => {
   class MyVotes {
     constructor() {
       this.mainEl = document.getElementsByClassName("main-container")[0];
+      this.votesEl = document.getElementsByClassName("user-votes")[0];
     }
     main() {
       let mainInstance = new Main();
@@ -22,6 +23,46 @@ window.onload = () => {
         </div>
       `
       this.mainEl.insertAdjacentHTML("afterbegin", profileInfoEl);
+      this.getUserVotes();
+    }
+    populateUserVotes(data) {
+      for (var i = 0; i < data.length; i++) {
+      let votesEl = `
+        <div class="vote">
+          <h3 class="text-color-design-primary">${ data[i].officeName }</h3>
+          <div class="candidate-index">
+              <div style="background-image: url('${ data[i].candidateUrl }');" class="candidate-profile-image-index"></div>
+              <div class="candidate-info-index">
+                <h3>${ data[i].candidateName }</h3>
+              </div>
+          </div>
+        </div>
+      `
+      this.votesEl.insertAdjacentHTML("afterbegin", votesEl);
+      }
+    }
+    async getUserVotes() {
+      try {
+        this.main().loading();
+        let user = await this.main().readFromDatabase();
+        console.log(user.id);
+        let votes = await this.main().performFetch("/votes");
+        for (var i = 0; i < votes.data.data.length; i++) {
+          if (user.id == votes.data.data[i].created_by) {
+            let candidate = await this.main().performFetch(`/user/${ votes.data.data[i].candidate }`)
+            votes.data.data[i]["candidateName"] = candidate.data.data[0].first_name + " " + candidate.data.data[0].last_name
+            votes.data.data[i]["candidateUrl"] = candidate.data.data[0].passport_url
+            let office = await this.main().performFetch(`/offices/${ votes.data.data[i].office }`)
+            votes.data.data[i]["officeName"] = office.data.data[0].name
+          }
+        }
+        this.main().loading(false);
+        this.populateUserVotes(votes.data.data);
+        console.log(votes.data.data);
+      } catch (error) {
+        this.main().loading(false);
+        console.log(error);
+      }
     }
     async getUserInfo() {
       try {
@@ -42,7 +83,7 @@ window.onload = () => {
 
     let logout = document.getElementById("logout");
     logout.addEventListener("click", () => {
-      votesInstance.main().navInstance().logout();
+      myVotesInstance.main().navInstance().logout();
     });
   })()
 }
